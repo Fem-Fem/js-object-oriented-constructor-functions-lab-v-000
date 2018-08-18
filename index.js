1b79c4afcc7c492aa7496dae2ca0ce9905a96263
Obafemi Fadairo <ofpupp@princeton.edu>
2018-08-18 22:01:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+scooter_contructor = function (year, color, model) {
+  this.year = year
+  this.color = color
+  this.model = model
+}
+
+scooter_contructor()
+
+let Scotter = new scooter_contructor
