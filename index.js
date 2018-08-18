b189f3410f0b87035dafb41a789007f0a9e1ae06
Obafemi Fadairo <ofpupp@princeton.edu>
2018-08-18 22:04:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ scooter_contructor = function (year, color, model) {
   this.model = model
 }
 
+driver_contructor = function (name, age, experience) {
+  this.name = name
+  this.age = age
+  this.experience = experience
+}
+
+
+reate a constructor function for each of the following:
+
+    Scooter with year, color, and model properties
+    Driver with name, age, and experience properties
+    PickupLocation with address and city properties
+
 scooter_contructor()
 
 let Scotter = new scooter_contructor
